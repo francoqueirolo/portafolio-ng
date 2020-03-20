5ebe21835a825a8d60ff42e1dbacebf34bff74b0
Franco Queirolo <fgqueirolo@gmail.com>
2020-03-20 00:42:22 -0500

    feat: search implements

---
+++ src/app/pages/search/search.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { ProductService } from "../../services/product.service";
+
+@Component({
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"]
+})
+export class SearchComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    public productService: ProductService
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      console.log(params["term"]);
+      this.productService.buscarProducto(params["termino"]);
+    });
+  }
+}
